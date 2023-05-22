bd6a698a8dd3ab074a2fd20f51c1856266e62746
AroKristen <125212882+AroKristen@users.noreply.github.com>
2023-05-22 10:35:54 +0530

    Add files via upload

--- 4.1/reg.php
+++ 4.1/reg.php
@@ -1,20 +1,21 @@
-<style>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Profile for instrument delivery</title>
+  <style>
   @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900');
-
   body {
     font-family: 'Poppins', sans-serif;
     font-weight: 300;
     font-size: 15px;
     line-height: 1.7;
-    color: #00008B;
-    background-color: 	#FFFFFF;
+    color: #c4c3ca;
+    background-color: #FFFF;
     overflow-x: hidden;
   }
-
   form {
     color: black;
   }
-
   .card {
     position: absolute;
     top: 50%;
@@ -31,7 +32,6 @@
     align-content: center;
     align-items: center;
   }
-
     .ncard {
     position: absolute;
     top: 80%;
@@ -48,44 +48,38 @@
     align-content: center;
     align-items: center;
   }
-
   .card h1 {
     margin-top: 0;
     font-size: 28px;
     color: #fff;
   }
-
   .card p {
     color: #fff;
   }
-</style>
+  </style>
+</head>
+<body> 
+    <form action="registration.php" method="post">
+        <h1>Registration</h1>
+  <label for="id">ID:</label>
+  <input type="text" id="id" name="id"  ><br>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" ><br>
+  <label for="email">Email:</label>
+  <input type="text" id="email" name="email" ><br>
+  <label for="pass">Password:</label>
+  <input type="password" id="pass" name="pass"><br>
+  <label for="country">Country:</label>
+  <input type="text" id="country" name="country" ><br>
+  <label for="city">City:</label>
+  <input type="text" id="city" name="city"><br>
 
-<h3>Detail retrieval using xml</h3>
-<h2> Welcome to MUSIC WORLD</h2>
-<p>retrive the instruments available with us</p>
-<form method="get">
-  <div class="card">
-  <label for="movieId">instrument ID:</label>
-  <input type="text" id="id" name="id"><br>
-  <button type="submit">Submit</button>
-</div>
+  <label for="contact">Contact:</label>
+  <input type="text" id="contact" name="contact"><br>
+  <label for="address">Address:</label>
+  <textarea id="address" name="address"></textarea><br>
+  <input type="submit" value="Submit">
 </form>
-
-<?php
-  $users = simplexml_load_file('users.xml');
-  if ($users === false) {
-    die('Error loading XML file');
-  }
-  $id = $_GET['id'];
-  $user = $users->xpath("/users/user[@id='$id']");
-  if (count($user) === 0) {
-    die('User not found');
-  }
-  $user = $user[0];
-  echo "<div class='ncard'>";
-  echo "<h1>{$user->Title}</h1>";
-  echo "<p>variant: {$user->variant}</p>";
-  echo "<p>Ratings: {$user->Ratings}</p>";
-  echo "</div>";
-?>
+</body>
+</html>
 
